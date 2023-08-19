b18d44a71b222c0e5b64231867b0facd50f282ec
Al McNicoll <github@almcnicoll.co.uk>
2023-08-19 23:30:08 +0100

    Added bootstrap, db class

--- inc/db.php
+++ inc/db.php
@@ -1,6 +1,26 @@
+<?php
+session_start();
 if (!@include_once('inc/secret.php')) {
     require_once('secret.php');
 }
 define('DB_HOST','localhost');
 define('DB_SCHEMA','destination-playlist');
 define('DB_USER','dp');
+// PASSWORD IN secret.php
+define('DB_CHARSET','utf8mb4');
+
+class db {
+    public function getDSN() {
+        return "mysql:host=".DB_HOST.";dbname=".DB_SCHEMA.";charset=".DB_CHARSET;
+    }
+
+    public function getPDO() {
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $dsn = self::getDSN();
+        return new PDO($dsn, DB_USER, DB_PASSWORD, $options);
+    }
+}
--- login.php
+++ login.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login to Destination Playlist</title>
+    <link href="css/bootstrap.min.css" cdn-src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js" cdn-src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+</head>
 <?php
-
+    require_once('inc/db.php');
 ?>
-Login page
+<html lang="en">
+    <div class="row">
+        <div class="span12">
+            <h1>Login page</h1>
+        </div>
+    </div>
+    <?php
+    //foreach auth method here
+    ?>
+</html>
