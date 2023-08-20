2e08d66715e1a12d6def774df14ba198831a9e35
Al McNicoll <github@almcnicoll.co.uk>
2023-08-20 10:54:00 +0100

    Switched to CDN for bootstrap, updated login page to read from db

--- login.php
+++ login.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login to Destination Playlist</title>
-    <link href="css/bootstrap.min.css" cdn-src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js" cdn-src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-</head>
 <?php
     require_once('class/authmethod.php');
 ?>
 <html lang="en">
-    <div class="row">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login to Destination Playlist</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <div class="row text-center">
         <div class="span12">
             <h1>Login page</h1>
         </div>
@@ -17,9 +18,10 @@
     <?php
     $auth_methods = AuthMethod::getAll();
     foreach ($auth_methods as $auth_method) {
-        echo "<div class='row'>\n";
-        echo "<div class='span12'><img src='{$auth_method->image}' /></div>\n";
+        echo "<div class='row text-center'>\n";
+        echo "<div class='span12'><a class='btn btn-lg' href='{$auth_method->handler}'><img src='{$auth_method->image}' height='60' /></a></div>\n";
         echo "</div>\n";
     }
     ?>
+</body>
 </html>
