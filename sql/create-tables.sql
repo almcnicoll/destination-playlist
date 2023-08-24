76bf7cffb1bdde08eb10d9aa09b351bd71a9e632
Al McNicoll <github@almcnicoll.co.uk>
2023-08-24 23:37:43 +0100

    Added letters table

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -37,3 +37,19 @@ CREATE TABLE `playlists` (
   CONSTRAINT `UserLookup` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 ;
+
+CREATE TABLE `letters` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `playlist_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `letter` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `spotify_track_id` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cached_artist` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cached_title` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ListIndex` (`playlist_id`,`letter`,`id`,`cached_artist`,`cached_title`),
+  CONSTRAINT `FK_playlist_lookup` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+;
