605b2a192715d3664f6dc6ba1e2f4c73e3035880
Al McNicoll <github@almcnicoll.co.uk>
2023-08-29 17:31:44 +0100

    Updated note about secrets on config.local.php

--- inc/config.local.php
+++ inc/config.local.php
@@ -3,17 +3,8 @@
 $config['root_path'] = 'http://localhost:8888/destination-playlist';
 
 // DATABASE CONFIG
-// TO DO - switch these to $config at some point
-/*
-define('DB_HOST','localhost');
-define('DB_SCHEMA','destination-playlist');
-define('DB_USER','dp');
-// PASSWORD IN secret.php
-define('DB_CHARSET','utf8mb4');
-*/
-
 $config['DB_HOST']      = 'localhost';
 $config['DB_SCHEMA']    ='destination-playlist';
 $config['DB_USER']      = 'dp';
-// PASSWORD IN secret.php
+// PASSWORD MUST BE SET IN secret.php, in the format $config['DB_PASSWORD'] = '';
 $config['DB_CHARSET']   = 'utf8mb4';
