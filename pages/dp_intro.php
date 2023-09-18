28ac30d670479f5ee2a97400b2c030f652745a6d
Al McNicoll <github@almcnicoll.co.uk>
2023-09-18 11:25:06 +0100

    Tweaked intro page
    Added detection for logged in / not, affecting first step

--- pages/dp_intro.php
+++ pages/dp_intro.php
@@ -8,13 +8,20 @@
 <div class='card'>
     <div class='card-body'>
         <h3 class='card-title'>How it works</h3>
-        <ol>
+        <ol class='fs-4'>
+<?php if (isset($_SESSION['USER'])): ?>
+            <li class='step-done'>Sign in / register using Spotify</li>
+<?php else: ?>
             <li><a href="<?= $config['root_path'] ?>/login.php">Sign in / register</a> using Spotify</li>
+            <div class="fs-6 step-explain">You'll need a Spotify account to use Destination Playlist</div>
+<?php endif; ?>
             <li>Enter the place you're going</li>
-            <li>Each letter of the destination becomes a launchpad for a track, using the track title or the artist</li>
+            <div class="fs-6 step-explain">Each letter of the destination becomes the start of a track</div>
             <li>Share the code with your travel buddies</li>
-            <li>Each friend gets a set of letters, and picks tracks</li>
-            <li>Result: an awesome playlist that broadens your musical horizons!</li>
+            <div class="fs-6 step-explain">Your unique code keeps the playlist between you and your friends</div>
+            <li>Everyone picks tracks based on the destination</li>
+            <div class="fs-6 step-explain">Which means that everyone discovers new music</div>
+            <li>Listen and discover together!</li>
         </ol>
     </div>
 </div>
