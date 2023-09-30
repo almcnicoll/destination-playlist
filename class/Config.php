7a4e1a34242580669682844f9ef8aa39d87aed3e
Al McNicoll <github@almcnicoll.co.uk>
2023-09-30 22:03:29 +0100

    Switched to PageInfo class for authentication management

--- class/Config.php
+++ class/Config.php
@@ -2,6 +2,7 @@
 
 class Config {
     private static $__config = [];
+    private static $__loaded = false;
 
     private static function retrieveSecrets() {
         $config = [];
@@ -28,10 +29,22 @@ class Config {
     }
 
     public static function init() {
+        if (self::$__loaded) { return; }
+
         self::retrieveSecrets();
         self::retrieveLocalConfig();
         // Add any non-local, non-secret config here in the form:
         // self::$__config['variable_key'] = 'variable value';
+
+        // When to perform user authentication on the specified pages
+        //  values are PAGEAUTH_NONE (don't authenticate), PAGEAUTH_LATE (authenticate after content rendered) or PAGE_AUTH_EARLY (authenticate first thing)
+        //  the default is 'early' and any unrecognised values will be handled as 'early'
+        // TODO - move this to separate Route/Page class?
+        self::$__config['pageinfo'] = [];
+        self::$__config['pageinfo']['index'] = new PageInfo(PageInfo::AUTH_EARLY, '/dp/intro');
+        self::$__config['pageinfo']['dp_intro'] = new PageInfo(PageInfo::AUTH_NEVER, false);
+
+        self::$__loaded = true; // Don't need to reinitialise
     }
 
     public static function get() {
--- class/User.php
+++ class/User.php
@@ -17,4 +17,49 @@ class User extends Model {
     public function getAuthmethod() : ?AuthMethod {
         return AuthMethod::getById($this->authmethod_id);
     }
+
+    public static function loginCheck($redirectOnFail = true) : bool {
+        $config = Config::get();
+
+        // $login_check_redirect_on_fail allows pages to redirect unauthenticated users to custom URLs (e.g. / -> /dp/intro)
+        // $login_check_soft_fail allows pages to refresh tokens if needed, but not redirect to login on fail (e.g. /dp/intro page, which is valid for unauthenticated users)
+        if(session_status() !== PHP_SESSION_ACTIVE) { session_start(); }
+
+        $currentUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+        if(!(
+        isset($_SESSION['USER']) &&
+        isset($_SESSION['USER_ID']) &&
+        isset($_SESSION['USER_AUTHMETHOD_ID']) &&
+        isset($_SESSION['USER_ACCESSTOKEN']) &&
+        isset($_SESSION['USER_REFRESHTOKEN']) &&
+        isset($_SESSION['USER_REFRESHNEEDED'])
+        )) {
+            // Need to log in
+            //echo "<pre>Session:\n".print_r($_SESSION,true)."</pre>";
+            if ($redirectOnFail !== false) {
+                if ($redirectOnFail === true) {
+                    header("Location: {$config['root_path']}/login.php?redirect_url=".urlencode($currentUrl));
+                } else {
+                    header("Location: {$config['root_path']}{$redirectOnFail}");
+                }
+                die();
+            }
+            return false;
+        } else {
+            // Check if our token is still valid
+            $refresh_needed = (int)($_SESSION['USER_REFRESHNEEDED']);
+            //die("<pre>Comparing {$refresh_needed} to ".time()."</pre>\n");
+            if ($refresh_needed < time()) {
+                // Call refresh mechanism
+                $method = AuthMethod::getById((int)$_SESSION['USER_AUTHMETHOD_ID']);
+                header("Location: {$config['root_path']}/{$method->handler}?refresh_needed=true&redirect_url=".urlencode($currentUrl));
+                die();
+            }
+            // Otherwise, everything is OK! Just ensure that USER property is correctly populated as a User object
+            $discard = new User(); // Ensure that User class is autoloaded
+            $_SESSION['USER'] = unserialize(serialize($_SESSION['USER']));
+            return true;
+        }
+    }
 }
