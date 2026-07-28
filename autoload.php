88bbd8dccedf919a40eff034f9e63f3bdf773250
almcnicoll <almcnicoll@gmail.com>
2026-07-28 20:38:46 +0100

    Improved autoloader
    Autoloader now works out its correct location instead of doing recursive parent calls

--- autoload.php
+++ autoload.php
@@ -6,32 +6,10 @@ class Autoloader
     {
         global $config;
         spl_autoload_register(function ($class) {
-            $file = 'class'.DIRECTORY_SEPARATOR.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
+            $file = __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
             if (file_exists($file)) {
                 require $file;
                 return true;
-            } else {
-                // Allow for us calling this one directory down (e.g. in ajax call)
-                $file = '../'.$file;
-                if (file_exists($file)) {
-                    require $file;
-                    return true;
-                } else {
-                    // Allow for us calling this two directories down
-                    $file = '../'.$file;
-                    if (file_exists($file)) {
-                        require $file;
-                        return true;
-                    } else {
-                        // Allow for us calling this three directories down
-                        $file = '../'.$file;
-                        if (file_exists($file)) {
-                            require $file;
-                            return true;
-                        }
-                        return false;
-                    }
-                }
             }
             return false;
         });
